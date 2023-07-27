56829729320c868ffaa963efea5f2e4fa24c3024
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-27 17:08:29 +0545

    TransactionController

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker_Web.Models;
+
+namespace Expense_Tracker_Web.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transaction.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Transaction());
+            else
+                return View(_context.Transaction.Find(id));
+        }
+
+        // POST: Transaction/AddOrEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                    _context.Add(transaction);
+                else
+                    _context.Update(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryId", transaction.CategoryId);
+            return View(transaction);
+        }
+
+       
+
+        // GET: Transaction/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Transaction == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transaction
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transaction == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transaction'  is null.");
+            }
+            var transaction = await _context.Transaction.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transaction.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+       
+    }
+}
